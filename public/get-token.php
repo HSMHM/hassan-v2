8951b037750ca4affd6f44f5562e2b8736e7c10a
Hassan Almalki <hassan@dcco.sa>
2026-04-12 19:59:36 +0300

    fix(token): refactor token retrieval process to use graph.facebook.com and improve response handling

--- public/get-token.php
+++ public/get-token.php
@@ -19,27 +19,42 @@ if (isset($_GET['code'])) {
     $data = json_decode($r1, true);
     echo "<h2>Short-lived:</h2><pre>$r1</pre>";
     
-// Step 2: GET with params in URL (not body)
+// Step 2: Try via graph.facebook.com instead
     if (isset($data['access_token'])) {
-        $url = 'https://graph.instagram.com/access_token'
-             . '?grant_type=ig_exchange_token'
-             . '&client_secret=' . urlencode($secret)
-             . '&access_token=' . urlencode($data['access_token']);
-        
-        $ch2 = curl_init();
-        curl_setopt($ch2, CURLOPT_URL, $url);
+        // Attempt 1: graph.facebook.com with ig_exchange_token
+        $url1 = 'https://graph.facebook.com/v25.0/access_token'
+              . '?grant_type=ig_exchange_token'
+              . '&client_secret=' . urlencode($secret)
+              . '&access_token=' . urlencode($data['access_token']);
+        $ch2 = curl_init($url1);
         curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch2, CURLOPT_HTTPGET, true);
-        curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch2, CURLOPT_HTTPHEADER, ['Accept: application/json']);
         $r2 = curl_exec($ch2);
-        $httpCode = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
-        $error = curl_error($ch2);
         curl_close($ch2);
+        echo "<h2>Attempt 1 (facebook/ig_exchange):</h2><pre>$r2</pre>";
         
-        echo "<h2>Long-lived (HTTP $httpCode):</h2><pre>$r2</pre>";
-        if ($error) echo "<h2>Curl Error:</h2><pre>$error</pre>";
+        // Attempt 2: graph.facebook.com with fb_exchange_token
+        $url2 = 'https://graph.facebook.com/v25.0/oauth/access_token'
+              . '?grant_type=fb_exchange_token'
+              . '&client_id=1328943562382309'
+              . '&client_secret=' . urlencode($secret)
+              . '&fb_exchange_token=' . urlencode($data['access_token']);
+        $ch3 = curl_init($url2);
+        curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+        $r3 = curl_exec($ch3);
+        curl_close($ch3);
+        echo "<h2>Attempt 2 (facebook/fb_exchange):</h2><pre>$r3</pre>";
+
+        // Attempt 3: graph.instagram.com with version
+        $url3 = 'https://graph.instagram.com/v25.0/access_token'
+              . '?grant_type=ig_exchange_token'
+              . '&client_secret=' . urlencode($secret)
+              . '&access_token=' . urlencode($data['access_token']);
+        $ch4 = curl_init($url3);
+        curl_setopt($ch4, CURLOPT_RETURNTRANSFER, true);
+        $r4 = curl_exec($ch4);
+        curl_close($ch4);
+        echo "<h2>Attempt 3 (instagram/v25.0):</h2><pre>$r4</pre>";
+        
         echo "<h2>User ID:</h2><pre>{$data['user_id']}</pre>";
     }
 } else {
